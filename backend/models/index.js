0980d0e1af07b4f0a32f565480bbb61253b1ea0e
Midanka Lahon <midankalahon@gmail.com>
2025-04-29 21:33:20 +0530

    Connected DB

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('lmsdb', 'postgres', 'Mymonster@786', {
-    host: 'localhost',
-    dialect: 'postgres'
+require('dotenv').config(); // Load environment variables from .env file
+
+const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST } = process.env; // Destructure environment variables
+const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+    host: PGHOST,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, 
+            rejectUnauthorized: false
+        }
+    }
 });
 
 sequelize.sync(); // creates tables if not exist
